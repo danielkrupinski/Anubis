0801debf74002563de0e03e8ee95cd4e21a52b3d
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-26 21:30:37 +0200

    Initialize interfaces on DLL_PROCESS_ATTACH

--- Anubis/dllmain.c
+++ Anubis/dllmain.c
@@ -1,5 +1,7 @@
 #include <Windows.h>
 
+#include "Interfaces.h"
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -8,6 +10,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        initializeInterfaces();
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
