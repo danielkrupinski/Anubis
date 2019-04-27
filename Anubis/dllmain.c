3a30847bc32a63d9cf8775751390c63a95c26961
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-27 12:17:43 +0200

    Simplify DllMain

--- Anubis/dllmain.c
+++ Anubis/dllmain.c
@@ -2,20 +2,12 @@
 
 #include "Interfaces.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD ul_reason_for_call,
+                      LPVOID lpReserved)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
         initializeInterfaces();
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
     return TRUE;
 }
 
