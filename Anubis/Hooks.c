b995883e29da225fa738969ae0b2bf1f1a616f33
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-27 18:17:15 +0200

    Add static size_t calculateLength(uintptr_t* vmt) function

--- Anubis/Hooks.c
+++ Anubis/Hooks.c
@@ -1,4 +1,17 @@
+#include <Windows.h>
+
 #include "Hooks.h"
+
+Hooks hooks;
+
+static size_t calculateLength(uintptr_t* vmt)
+{
+    size_t length = 0;
+    MEMORY_BASIC_INFORMATION memoryInfo;
+    while (VirtualQuery((LPVOID)vmt[length], &memoryInfo, sizeof(memoryInfo)) && memoryInfo.Protect == PAGE_EXECUTE_READ)
+        length++;
+    return length;
+}
 
 void initializeHooks(void)
 {
