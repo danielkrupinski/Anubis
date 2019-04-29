874f25d2567a4c155dafbb5e73cb9362e180126d
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-29 13:23:33 +0200

    Add Memory::present pointer

--- Anubis/Memory.h
+++ Anubis/Memory.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <stdint.h>
+
 typedef struct {
     void** clientMode;
     void(__fastcall* loadSky)(const char*);
+    void* present;
 } Memory;
 
 void initializeMemory(void);
