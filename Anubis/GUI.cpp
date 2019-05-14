400d8e08458fa2a25f3d2c95c0802d62508c0c87
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-14 20:15:46 +0200

    Move GUI rendering and input handling to GUI.cpp

--- Anubis/GUI.cpp
+++ Anubis/GUI.cpp
@@ -1,6 +1,67 @@
+#include <d3d9.h>
 #include "GUI.h"
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_D3D9_IMPLEMENTATION
+#include "nuklear/nuklear.h"
+#include "nuklear/nuklear_d3d9.h"
+
+struct nk_context* ctx;
+
+bool GUI_handleInput(HWND window, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    return nk_d3d9_handle_event(window, msg, wParam, lParam);
+}
+
+void GUI_init(struct IDirect3DDevice9* device)
+{
+    ctx = nk_d3d9_init(device, 1280, 720);
+    struct nk_font_atlas* atlas;
+    nk_d3d9_font_stash_begin(&atlas);
+    nk_d3d9_font_stash_end();
+}
 
 void GUI_render(void)
 {
+    nk_input_end(ctx);
 
+    if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
+        NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE |
+        NK_WINDOW_MINIMIZABLE | NK_WINDOW_TITLE))
+    {
+        enum { EASY, HARD };
+        static int op = EASY;
+        static int property = 20;
+
+        nk_layout_row_dynamic(ctx, 30, 2);
+        if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
+        if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
+        nk_layout_row_dynamic(ctx, 22, 1);
+        nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+
+        nk_layout_row_dynamic(ctx, 20, 1);
+        nk_label(ctx, "background:", NK_TEXT_LEFT);
+        nk_layout_row_dynamic(ctx, 25, 1);
+        static struct nk_colorf bg = { 0.1f, 0.18f, 0.24f, 1.0f };
+
+        if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx), 400))) {
+            nk_layout_row_dynamic(ctx, 120, 1);
+            bg = nk_color_picker(ctx, bg, NK_RGBA);
+            nk_layout_row_dynamic(ctx, 25, 1);
+            bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f, 0.005f);
+            bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f, 0.005f);
+            bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f, 0.005f);
+            bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f, 0.005f);
+            nk_combo_end(ctx);
+        }
+    }
+    nk_end(ctx);
+    nk_d3d9_render(NK_ANTI_ALIASING_ON);
+    nk_input_begin(ctx);
 }
--- Anubis/GUI.h
+++ Anubis/GUI.h
@@ -1,3 +1,13 @@
 #pragma once
 
+#include <stdbool.h>
+
+struct IDirect3DDevice9;
+
+bool GUI_handleInput(HWND, UINT, WPARAM, LPARAM);
+void GUI_init(struct IDirect3DDevice9*);
 void GUI_render(void);
+
+struct nk_context;
+extern struct nk_context* ctx;
+// nk_d3d9_handle_event(window, msg, wParam, lParam)
