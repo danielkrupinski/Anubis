2ee273ef71610ed39830e3261ebc82c89e35db2b
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-03 20:28:09 +0200

    Remove nuklear library

--- Anubis/GUI.cpp
+++ Anubis/GUI.cpp
@@ -1,20 +1,11 @@
 #include <d3d9.h>
 #include <Windows.h>
+
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_win32.h"
 #include "imgui/imgui_impl_dx9.h"
+
 #include "GUI.h"
-#define NK_INCLUDE_FIXED_TYPES
-#define NK_INCLUDE_STANDARD_IO
-#define NK_INCLUDE_STANDARD_VARARGS
-#define NK_INCLUDE_DEFAULT_ALLOCATOR
-#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-#define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
-#define NK_IMPLEMENTATION
-#define NK_D3D9_IMPLEMENTATION
-#include "nuklear/nuklear.h"
-#include "nuklear/nuklear_d3d9.h"
 
 bool isGuiOpen = false;
 
