2bc3154a20a65ced4c4a455c3f78387e737f8507
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-05 19:32:03 +0200

    Use wide char version of GetModuleHandle() function

--- Anubis/Interfaces.c
+++ Anubis/Interfaces.c
@@ -6,10 +6,11 @@
 
 Interfaces interfaces;
 
-static void* find(const char* module, const char* name)
+static uintptr_t** find(const wchar_t* module, const char* name)
 {
-    void**(*createInterface)(const char*, int*) = (void**(*)(const char*, int*))GetProcAddress(GetModuleHandle(module), "CreateInterface");
-    void** foundInterface = 0;
+    typedef uintptr_t** (*CreateInterface)(const char*, int*);
+    CreateInterface createInterface = (CreateInterface)GetProcAddress(GetModuleHandleW(module), "CreateInterface");
+    uintptr_t** foundInterface = 0;
 
     if (createInterface)
         foundInterface = createInterface(name, 0);
@@ -18,7 +19,7 @@ static void* find(const char* module, const char* name)
         return foundInterface;
     else {
         char buf[100];
-        sprintf_s(buf, sizeof(buf), "Failed to find %s in %s!", name, module);
+        sprintf_s(buf, sizeof(buf), "Failed to find %s interface!", name);
         MessageBox(NULL, buf, "Error", MB_OK | MB_ICONERROR);
         exit(EXIT_FAILURE);
     }
@@ -26,11 +27,11 @@ static void* find(const char* module, const char* name)
 
 void initializeInterfaces(void)
 {
-    interfaces.client = find("client_panorama", "VClient018");
-    interfaces.engine = find("engine", "VEngineClient014");
-    interfaces.entityList = find("client_panorama", "VClientEntityList003");
-    interfaces.gameUI = find("client_panorama", "GameUI011");
-    interfaces.surface = find("vguimatsurface", "VGUI_Surface031");
+    interfaces.client = find(L"client_panorama", "VClient018");
+    interfaces.engine = find(L"engine", "VEngineClient014");
+    interfaces.entityList = find(L"client_panorama", "VClientEntityList003");
+    interfaces.gameUI = find(L"client_panorama", "GameUI011");
+    interfaces.surface = find(L"vguimatsurface", "VGUI_Surface031");
 
     GameUI_messageBox("Huge success!", "Anubis has been succesfully loaded.");
 }
