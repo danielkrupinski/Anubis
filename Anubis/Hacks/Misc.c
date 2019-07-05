9f135617cc11a921bf7baf9fcfca279a9e27da67
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-05 19:31:28 +0200

    Check config variables in Misc.c

--- Anubis/Hacks/Misc.c
+++ Anubis/Hacks/Misc.c
@@ -1,16 +1,17 @@
+#include "../Config.h"
 #include "Misc.h"
 #include "../SDK/Engine.h"
 #include "../SDK/EntityList.h"
 
 void Misc_bunnyhop(UserCmd* cmd)
 {
-    if (!(*((int*)((char*)EntityList_getEntity(Engine_getLocalPlayer()) + 0x104)) & 1))
+    if (config.misc.bunnyhop && !(*((int*)((char*)EntityList_getEntity(Engine_getLocalPlayer()) + 0x104)) & 1))
         cmd->buttons &= ~IN_JUMP;
 }
 
 void Misc_autostrafe(UserCmd* cmd)
 {
-    if (!(*((int*)((char*)EntityList_getEntity(Engine_getLocalPlayer()) + 0x104)) & 1)) {
+    if (config.misc.autostrafe && !(*((int*)((char*)EntityList_getEntity(Engine_getLocalPlayer()) + 0x104)) & 1)) {
         if (cmd->mousedx < 0)
             cmd->sidemove = -450.0f;
         else if (cmd->mousedx > 0)
