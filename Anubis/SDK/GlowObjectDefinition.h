1b49e18522919c6f178ca418c1d7dce069f5c607
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-16 19:53:23 +0200

    Add GlowObjectDefinition struct

--- Anubis/SDK/GlowObjectDefinition.h
+++ Anubis/SDK/GlowObjectDefinition.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include <Windows.h>
+
+#include "Vector.h"
+
+struct GlowObjectDefinition {
+    PVOID entity;
+    Vector glowColor;
+    float   alpha;
+    BYTE pad[4];
+    float   m_flSomeFloat;
+    float   bloomAmount;
+    float   m_flAnotherFloat;
+    bool    renderWhenOccluded;
+    bool    renderWhenUnoccluded;
+    bool    fullBloomRender;
+    BYTE pad1;
+    int fullBloomStencilTestValue;
+    int glowStyle;
+    int splitScreenSlot;
+    int nextFreeSlot;
+};
