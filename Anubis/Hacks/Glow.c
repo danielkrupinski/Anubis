63d0cda3b99e35c04837ea67fb081861785dc1b1
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-24 19:39:31 +0200

    Add applyGlow() function

--- Anubis/Hacks/Glow.c
+++ Anubis/Hacks/Glow.c
@@ -2,6 +2,22 @@
 #include "../Memory.h"
 #include "../SDK/Entity.h"
 #include "../SDK/GlowObjectDefinition.h"
+#include "../SDK/ClientClass.h"
+#include "../SDK/ClassId.h"
+#include "../Config.h"
+
+static VOID applyGlow(GlowObjectDefinition* glowObject, GlowConfig* glowConfig, INT health)
+{
+    if (glowConfig->enabled) {
+        glowObject->renderWhenOccluded = true;
+        glowObject->alpha = glowConfig->alpha;
+        glowObject->glowStyle = glowConfig->style;
+        glowObject->bloomAmount = glowConfig->thickness;
+        glowObject->glowColor.x = glowConfig->color[0];
+        glowObject->glowColor.y = glowConfig->color[1];
+        glowObject->glowColor.z = glowConfig->color[2];
+    }
+}
 
 VOID Glow_render(VOID)
 {
@@ -11,13 +27,17 @@ VOID Glow_render(VOID)
 
         if (glowObject->nextFreeSlot != -2 || !entity || Entity_isDormant(entity))
             continue;
-        glowObject->renderWhenOccluded = true;
-        glowObject->renderWhenUnoccluded = false;
-        glowObject->alpha = 1.0f;
-        glowObject->glowStyle = 0;
-        glowObject->bloomAmount = 1.0f;
-        glowObject->glowColor.x = 1.0f;
-        glowObject->glowColor.y = 1.0f;
-        glowObject->glowColor.z = 1.0f;
+
+        switch (Entity_getClientClass(entity)->classId) {
+        case ClassId_CSPlayer:
+            glowObject->renderWhenOccluded = true;
+            glowObject->alpha = 1.0f;
+            glowObject->glowStyle = 0;
+            glowObject->bloomAmount = 1.0f;
+            glowObject->glowColor.x = 1.0f;
+            glowObject->glowColor.y = 1.0f;
+            glowObject->glowColor.z = 1.0f;
+            break;
+        }
     }
 }
