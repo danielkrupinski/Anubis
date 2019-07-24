b020c148c1450c17db4f4e4e6de5fd64a885e6e9
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-24 15:51:47 +0200

    Create ClassId.h

---
+++ Anubis/SDK/ClassId.h
@@ -0,0 +1,25 @@
+#pragma once
+
+enum ClassId {
+    ClassId_C4 = 34,
+    ClassId_Chicken = 36,
+    ClassId_CSPlayer = 40,
+    ClassId_Deagle = 46,
+    ClassId_Knife = 107,
+    ClassId_KnifeGG,
+    ClassId_PlantedC4 = 128,
+    ClassId_PropDoorRotating = 142,
+    ClassId_Tablet = 171,
+    ClassId_Aug = 231,
+    ClassId_Awp,
+    ClassId_Elite = 238,
+    ClassId_FiveSeven = 240,
+    ClassId_G3sg1,
+    ClassId_Glock = 244,
+    ClassId_P2000,
+    ClassId_P250 = 257,
+    ClassId_Scar20 = 260,
+    ClassId_Sg553 = 264,
+    ClassId_Ssg08 = 266,
+    ClassId_Tec9 = 268
+};
