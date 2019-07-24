51fd35a4141e1ef337830bae1785cee8a82dee30
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-24 15:47:25 +0200

    Add ClientClass struct

--- Anubis/SDK/ClientClass.h
+++ Anubis/SDK/ClientClass.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include <Windows.h>
+
+typedef struct ClientClass {
+    PVOID(*createFunction)(INT, INT);
+    PVOID createEventFunction;
+    PSTR networkName;
+    PVOID recvTable;
+    ClientClass* next;
+    INT classId;
+} ClientClass;
