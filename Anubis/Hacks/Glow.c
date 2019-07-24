31c6db553e2bcb6b6924948669694f8170bec77a
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-24 15:21:21 +0200

    Update glow

--- Anubis/Hacks/Glow.c
+++ Anubis/Hacks/Glow.c
@@ -1,6 +1,23 @@
 #include "Glow.h"
+#include "../Memory.h"
+#include "../SDK/Entity.h"
+#include "../SDK/GlowObjectDefinition.h"
 
 VOID Glow_render(VOID)
 {
+    for (int i = 0; i < memory.glowObjectManager->glowObjectDefinitions.size; i++) {
+        GlowObjectDefinition* glowObject = (GlowObjectDefinition*)memory.glowObjectManager->glowObjectDefinitions.memory + i;
+        PVOID entity = glowObject->entity;
 
+        if (glowObject->nextFreeSlot != -2 || !entity || Entity_isDormant(entity))
+            continue;
+        glowObject->renderWhenOccluded = true;
+        glowObject->renderWhenUnoccluded = false;
+        glowObject->alpha = 1.0f;
+        glowObject->glowStyle = 0;
+        glowObject->bloomAmount = 1.0f;
+        glowObject->glowColor.x = 1.0f;
+        glowObject->glowColor.y = 1.0f;
+        glowObject->glowColor.z = 1.0f;
+    }
 }
--- Anubis/SDK/GlowObjectDefinition.h
+++ Anubis/SDK/GlowObjectDefinition.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdbool.h>
 #include <Windows.h>
 
 #include "UtlVector.h"
