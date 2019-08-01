4ac9c097035be391eea8be4f572a7d0ac4019672
Daniel Krupiński <danielkrupinski@outlook.com>
2019-08-01 20:50:27 +0200

    Update Entity.c

--- Anubis/SDK/Entity.c
+++ Anubis/SDK/Entity.c
@@ -6,27 +6,27 @@
 
 ClientClass* Entity_getClientClass(PVOID entity)
 {
-    CALL_VIRTUAL_METHOD_RETURN(ClientClass*(__fastcall*)(PVOID, PVOID), ((PUINT*)entity + 2), 2);
+    CALL_VIRTUAL_METHOD_RETURN(ClientClass*(__fastcall*)(PVOID, PVOID), (PUINT*)entity + 2, 2);
 }
 
 bool Entity_isDormant(PVOID entity)
 {
-    CALL_VIRTUAL_METHOD_RETURN(bool(__fastcall*)(PVOID, PVOID), ((PUINT*)entity + 2), 9);
+    CALL_VIRTUAL_METHOD_RETURN(bool(__fastcall*)(PVOID, PVOID), (PUINT*)entity + 2, 9);
 }
 
 bool Entity_setupBones(PVOID entity, Matrix3x4* out, INT maxBones, INT boneMask, FLOAT currentTime)
 {
-    CALL_VIRTUAL_METHOD_RETURN(bool(__fastcall*)(PVOID, PVOID, Matrix3x4*, INT, INT, FLOAT), ((PUINT*)entity + 1), 13, out, maxBones, boneMask, currentTime);
+    CALL_VIRTUAL_METHOD_RETURN(bool(__fastcall*)(PVOID, PVOID, Matrix3x4*, INT, INT, FLOAT), (PUINT*)entity + 1, 13, out, maxBones, boneMask, currentTime);
 }
 
 bool Entity_isWeapon(PVOID entity)
 {
-    CALL_VIRTUAL_METHOD_RETURN(bool(__fastcall*)(PVOID, PVOID), ((PUINT*)entity), 163);
+    CALL_VIRTUAL_METHOD_RETURN(bool(__fastcall*)(PVOID, PVOID), (PUINT*)entity, 163);
 }
 
 VOID Entity_getEyePosition(PVOID entity, Vector* out)
 {
-    CALL_VIRTUAL_METHOD(VOID(__fastcall*)(PVOID, PVOID, Vector*), ((PUINT*)entity), 281, out);
+    CALL_VIRTUAL_METHOD(VOID(__fastcall*)(PVOID, PVOID, Vector*), (PUINT*)entity, 281, out);
 }
 
 bool Entity_isEnemy(PVOID entity)
