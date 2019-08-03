dc4031e08b7c3ff5da004dfc2565f153d63d3c32
Daniel Krupiński <danielkrupinski@outlook.com>
2019-08-03 17:39:52 +0200

    Add Recv structs

--- Anubis/SDK/Recv.h
+++ Anubis/SDK/Recv.h
@@ -1,1 +1,43 @@
 #pragma once
+
+#include <Windows.h>
+#include "Vector.h"
+
+typedef struct RecvProxyData {
+    INT pad;
+    union {
+        FLOAT _float;
+        INT _int;
+        PSTR _string;
+        PVOID data;
+        Vector vector;
+        INT64 int64;
+    } value;
+} RecvProxyData;
+
+typedef struct RecvProp {
+    PSTR name;
+    INT type;
+    INT flags;
+    INT stringBufferSize;
+    INT insideArray;
+    LPCVOID extraData;
+    struct RecvProp* arrayProp;
+    PVOID arrayLengthProxy;
+    VOID(*proxy)(RecvProxyData*, PVOID, PVOID);
+    PVOID dataTableProxy;
+    struct RecvTable* dataTable;
+    INT offset;
+    INT elementStride;
+    INT elementCount;
+    PCSTR parentArrayPropName;
+} RecvProp;
+
+typedef struct RecvTable {
+    RecvProp* props;
+    INT propCount;
+    PVOID decoder;
+    PSTR netTableName;
+    bool isInitialized;
+    bool isInMainList;
+} RecvTable;
