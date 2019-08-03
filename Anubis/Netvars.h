bc7c412f04364c97fa3e9e9a0dbd2877f6e8644e
Daniel Krupiński <danielkrupinski@outlook.com>
2019-08-03 17:51:24 +0200

    Add netvar creation macros

--- Anubis/Netvars.h
+++ Anubis/Netvars.h
@@ -4,3 +4,18 @@
 
 VOID Netvars_init(VOID);
 SIZE_T Netvars_getOffset(UINT);
+
+#define NETVAR_OFFSET(funcname, class_name, var_name, offset, type) \
+type* Entity_##funcname(PVOID entity);
+
+#define NETVAR(funcname, class_name, var_name, type) \
+	NETVAR_OFFSET(funcname, class_name, var_name, 0, type)
+
+#define NETVAR_OFFSET_IMPL(funcname, class_name, var_name, offset, type) \
+type* Entity_##funcname(PVOID entity) \
+{ \
+	return (type*)((PBYTE)entity + Netvars_getOffset(UTILS_HASH(class_name "->" var_name)) + offset); \
+}
+
+#define NETVAR_IMPL(funcname, class_name, var_name, type) \
+	NETVAR_OFFSET_IMPL(funcname, class_name, var_name, 0, type)
