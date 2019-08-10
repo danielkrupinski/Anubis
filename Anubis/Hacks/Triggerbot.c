a0115fc0c4426f3a32dc92de00ead2302a528b48
Daniel Krupiński <danielkrupinski@outlook.com>
2019-08-10 20:39:23 +0200

    Update Triggerbot_run() function

--- Anubis/Hacks/Triggerbot.c
+++ Anubis/Hacks/Triggerbot.c
@@ -1,13 +1,20 @@
 #include "../Config.h"
 #include "../Memory.h"
 
+#include "../SDK/ConVar.h"
+#include "../SDK/Cvar.h"
 #include "../SDK/Engine.h"
 #include "../SDK/Entity.h"
 #include "../SDK/EntityList.h"
 #include "../SDK/GlobalVars.h"
 #include "../SDK/ItemDefinitionIndex.h"
+#include "../SDK/UserCmd.h"
+#include "../SDK/Utils.h"
+#include "../SDK/Vector.h"
 
 #include "Triggerbot.h"
+
+#define MAX_RANGE 8192.0f
 
 VOID Triggerbot_run(UserCmd* cmd)
 {
@@ -33,6 +40,17 @@ VOID Triggerbot_run(UserCmd* cmd)
 
         if ((GetAsyncKeyState(config.triggerbot[weaponIndex].key) || !config.triggerbot[weaponIndex].onKey)
             && now - lastTime >= config.triggerbot[weaponIndex].shotDelay / 1000.0f) {
+
+            UTILS_STATIC_VAR(ConVar*, weaponRecoilScale, Cvar_findVar("weapon_recoil_scale"));
+
+            Vector aimPunch = *Entity_aimPunchAngle(localPlayer);
+            aimPunch.x *= ConVar_getFloat(weaponRecoilScale);
+            aimPunch.y *= ConVar_getFloat(weaponRecoilScale);
+            aimPunch.z *= ConVar_getFloat(weaponRecoilScale);
+
+            Vector viewAngles = { cosf(DEG2RAD(cmd->viewangles.x + aimPunch.x)) * cosf(DEG2RAD(cmd->viewangles.y + aimPunch.y)) * MAX_RANGE,
+                                  cosf(DEG2RAD(cmd->viewangles.x + aimPunch.x)) * sinf(DEG2RAD(cmd->viewangles.y + aimPunch.y)) * MAX_RANGE,
+                                 -sinf(DEG2RAD(cmd->viewangles.x + aimPunch.x)) * MAX_RANGE };
         }
     }
 }
