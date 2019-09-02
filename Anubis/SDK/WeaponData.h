2167b58c382dfdc5d681abe2a586fadef997fab8
Daniel Krupiński <danielkrupinski@outlook.com>
2019-09-02 19:56:25 +0200

    Add WeaponData struct

--- Anubis/SDK/WeaponData.h
+++ Anubis/SDK/WeaponData.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include <Windows.h>
+
+typedef struct WeaponData {
+    BYTE pad[20];
+    int maxClip;
+    BYTE pad1[176];
+    int type;
+    BYTE pad2[32];
+    bool fullAuto;
+    BYTE pad3[3];
+    int damage;
+    float armorRatio;
+    int bullets;
+    float penetration;
+    BYTE pad4[8];
+    float range;
+    float rangeModifier;
+    BYTE pad5[16];
+    bool hasSilencer;
+} WeaponData;
