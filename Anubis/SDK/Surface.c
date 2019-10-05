d1aa1bf109ade954c768e0083bbb1e48c49b7d1f
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-05 17:30:08 +0200

    Add Surface_setDrawColor2() function

--- Anubis/SDK/Surface.c
+++ Anubis/SDK/Surface.c
@@ -7,6 +7,11 @@ VOID Surface_setDrawColor(INT r, INT g, INT b, INT a)
     CALL_VIRTUAL_METHOD(VOID(__fastcall*)(PVOID, PVOID, INT, INT, INT, INT), interfaces.surface, 15, r, g, b, a);
 }
 
+VOID Surface_setDrawColor2(FLOAT color[3], INT a)
+{
+    CALL_VIRTUAL_METHOD(VOID(__fastcall*)(PVOID, PVOID, INT, INT, INT, INT), interfaces.surface, 15, (INT)(color[0] * 255), (INT)(color[1] * 255), (INT)(color[2] * 255), a);
+}
+
 VOID Surface_unlockCursor(VOID)
 {
     CALL_VIRTUAL_METHOD(VOID(__fastcall*)(PVOID, PVOID), interfaces.surface, 66);
--- Anubis/SDK/Surface.h
+++ Anubis/SDK/Surface.h
@@ -3,4 +3,5 @@
 #include <Windows.h>
 
 VOID Surface_setDrawColor(INT, INT, INT, INT);
+VOID Surface_setDrawColor2(FLOAT[3], INT);
 VOID Surface_unlockCursor(VOID);
