318d4750092010dd5c1acf33ab975655bec1f368
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-05 21:29:14 +0200

    Add Vector_transform() function

--- Anubis/SDK/Vector.c
+++ Anubis/SDK/Vector.c
@@ -1,4 +1,6 @@
 #include <math.h>
+
+#include "Matrix3x4.h"
 #include "Vector.h"
 
 VOID Angles_normalize(struct Vector* vec)
@@ -8,19 +10,28 @@ VOID Angles_normalize(struct Vector* vec)
     vec->z = 0.0f;
 }
 
-struct Vector Vector_multiply(struct Vector CONST* v1, struct Vector CONST* v2)
+struct Vector Vector_multiply(CONST struct Vector* v1, CONST struct Vector* v2)
 {
     struct Vector result = { v1->x * v2->x, v1->y * v2->y, v1->z * v2->z };
     return result;
 }
 
-struct Vector Vector_add(struct Vector CONST* v1, struct Vector CONST* v2)
+struct Vector Vector_add(CONST struct Vector* v1, CONST struct Vector* v2)
 {
     struct Vector result = { v1->x + v2->x, v1->y + v2->y, v1->z + v2->z };
     return result;
 }
 
-FLOAT Vector_dotProduct(struct Vector CONST* v1, struct Vector CONST* v2)
+FLOAT Vector_dotProduct(CONST struct Vector* v1, CONST struct Vector* v2)
 {
     return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;
+}
+
+struct Vector Vector_transform(CONST struct Vector* vec, CONST struct Matrix3x4* mat)
+{
+    struct Vector x = { mat->m[0][0], mat->m[0][1], mat->m[0][2] },
+                  y = { mat->m[1][0], mat->m[1][1], mat->m[1][2] },
+                  z = { mat->m[2][0], mat->m[2][1], mat->m[2][2] };
+    struct Vector result = { Vector_dotProduct(vec, &x) + mat->m[0][3], Vector_dotProduct(vec, &y) + mat->m[1][3], Vector_dotProduct(vec, &z) + mat->m[2][3] };
+    return result;
 }
--- Anubis/SDK/Vector.h
+++ Anubis/SDK/Vector.h
@@ -2,12 +2,15 @@
 
 #include <Windows.h>
 
+struct Matrix3x4;
+
 struct Vector {
     FLOAT x, y, z;
 };
 
 VOID Angles_normalize(struct Vector*);
 
-struct Vector Vector_multiply(struct Vector CONST*, struct Vector CONST*);
-struct Vector Vector_add(struct Vector CONST*, struct Vector CONST*);
-FLOAT Vector_dotProduct(struct Vector CONST*, struct Vector CONST*);
+struct Vector Vector_multiply(CONST struct Vector*, CONST struct Vector*);
+struct Vector Vector_add(CONST struct Vector*, CONST struct Vector*);
+FLOAT Vector_dotProduct(CONST struct Vector*, CONST struct Vector*);
+struct Vector Vector_transform(CONST struct Vector*, CONST struct Matrix3x4*);
