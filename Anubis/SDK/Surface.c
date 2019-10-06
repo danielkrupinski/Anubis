c4235f59c78df3cff313ffdb4f69b7911bea8e30
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-06 20:28:00 +0200

    Add Surface_drawOutlinedRect() function

--- Anubis/SDK/Surface.c
+++ Anubis/SDK/Surface.c
@@ -12,6 +12,11 @@ VOID Surface_setDrawColor2(FLOAT color[3], INT a)
     CALL_VIRTUAL_METHOD(VOID(__fastcall*)(PVOID, PVOID, INT, INT, INT, INT), interfaces.surface, 15, (INT)(color[0] * 255), (INT)(color[1] * 255), (INT)(color[2] * 255), a);
 }
 
+VOID Surface_drawOutlinedRect(FLOAT x0, FLOAT y0, FLOAT x1, FLOAT y1)
+{
+    CALL_VIRTUAL_METHOD(VOID(__fastcall*)(PVOID, PVOID, INT, INT, INT, INT), interfaces.surface, 18, (INT)x0, (INT)y0, (INT)x1, (INT)y1);
+}
+
 VOID Surface_getScreenSize(INT* width, INT* height)
 {
     CALL_VIRTUAL_METHOD(VOID(__fastcall*)(PVOID, PVOID, INT*, INT*), interfaces.surface, 44, width, height);
--- Anubis/SDK/Surface.h
+++ Anubis/SDK/Surface.h
@@ -4,5 +4,6 @@
 
 VOID Surface_setDrawColor(INT, INT, INT, INT);
 VOID Surface_setDrawColor2(FLOAT[3], INT);
+VOID Surface_drawOutlinedRect(FLOAT, FLOAT, FLOAT, FLOAT);
 VOID Surface_getScreenSize(INT*, INT*);
 VOID Surface_unlockCursor(VOID);
