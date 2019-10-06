e9a9afe0ff026571aa6fcfcbd6e853c1d2731d09
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-06 19:51:28 +0200

    Add DebugOverlay_screenPosition() function

---
+++ Anubis/SDK/DebugOverlay.c
@@ -0,0 +1,8 @@
+#include "DebugOverlay.h"
+#include "../Interfaces.h"
+#include "Utils.h"
+
+BOOL DebugOverlay_screenPosition(CONST struct Vector* point, struct Vector* screen)
+{
+    return CALL_VIRTUAL_METHOD(BOOL(__fastcall*)(PVOID, PVOID, CONST struct Vector*, struct Vector*), interfaces.debugOverlay, 13, point, screen);
+}
--- Anubis/SDK/DebugOverlay.h
+++ Anubis/SDK/DebugOverlay.h
@@ -1,2 +1,7 @@
 #pragma once
 
+#include <Windows.h>
+
+struct Vector;
+
+BOOL DebugOverlay_screenPosition(CONST struct Vector*, struct Vector*);
