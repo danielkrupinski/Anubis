af56adb9345fb9d557090ddb12d5fe0808b435e8
Daniel Krupiński <danielkrupinski@outlook.com>
2019-10-07 19:06:49 +0200

    Update boundingBox() function to generate vertices without use of array

--- Anubis/Hacks/Esp.c
+++ Anubis/Hacks/Esp.c
@@ -16,17 +16,6 @@ static BOOLEAN boundingBox(PVOID entity, struct BoundingBox* out)
     CONST struct Vector* min = Entity_obbMins(entity);
     CONST struct Vector* max = Entity_obbMaxs(entity);
 
-    const struct Vector points[] = {
-        { min->x, min->y, min->z },
-        { min->x, max->y, min->z },
-        { max->x, max->y, min->z },
-        { max->x, min->y, min->z },
-        { min->x, min->y, max->z },
-        { min->x, max->y, max->z },
-        { max->x, max->y, max->z },
-        { max->x, min->y, max->z }
-    };
-
     INT width, height;
     Surface_getScreenSize(&width, &height);
 
@@ -36,28 +25,32 @@ static BOOLEAN boundingBox(PVOID entity, struct BoundingBox* out)
     out->y1 = -(FLOAT)(height * 2);
 
     for (INT i = 0; i < 8; i++) {
-        struct Vector screenPoint;
-        struct Vector transformed = Vector_transform(points + i, Entity_coordinateFrame(entity));
+        CONST struct Vector vertex = { i & 1 ? max->x : min->x,
+                                       i & 2 ? max->y : min->y,
+                                       i & 4 ? max->z : min->z };
 
-        if (DebugOverlay_screenPosition(&transformed, &screenPoint))
+        struct Vector point;
+        struct Vector transformed = Vector_transform(&vertex, Entity_coordinateFrame(entity));
+
+        if (DebugOverlay_screenPosition(&transformed, &point))
             return FALSE;
 
-        if (out->x0 > screenPoint.x)
-            out->x0 = screenPoint.x;
+        if (out->x0 > point.x)
+            out->x0 = point.x;
 
-        if (out->y0 > screenPoint.y)
-            out->y0 = screenPoint.y;
+        if (out->y0 > point.y)
+            out->y0 = point.y;
 
-        if (out->x1 < screenPoint.x)
-            out->x1 = screenPoint.x;
+        if (out->x1 < point.x)
+            out->x1 = point.x;
 
-        if (out->y1 < screenPoint.y)
-            out->y1 = screenPoint.y;
+        if (out->y1 < point.y)
+            out->y1 = point.y;
     }
     return TRUE;
 }
 
 VOID Esp_render(VOID)
 {
-
+    
 }
