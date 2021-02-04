e00094a0a2c617dba22ad6eacf4aca693a7d41fc
Daniel Krupiński <danielkrupinski@outlook.com>
2021-02-04 13:53:18 +0100

    Move VmtHook and Hooks structs definitions to Hooks.c

--- Anubis/Hooks.c
+++ Anubis/Hooks.c
@@ -20,6 +20,22 @@
 #include "SDK/UserCmd.h"
 #include "SDK/Utils.h"
 
+struct VmtHook {
+    PVOID base;
+    PUINT_PTR oldVmt;
+    PUINT_PTR newVmt;
+    SIZE_T length;
+};
+
+struct Hooks {
+    HRESULT(WINAPI* originalPresent)(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*);
+    HRESULT(WINAPI* originalReset)(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*);
+    WNDPROC originalWndProc;
+    struct VmtHook clientMode;
+    struct VmtHook panel;
+    struct VmtHook surface;
+};
+
 static struct Hooks hooks;
 
 static LRESULT WINAPI hookedWndProc(HWND window, UINT msg, WPARAM wParam, LPARAM lParam)
--- Anubis/Hooks.h
+++ Anubis/Hooks.h
@@ -3,22 +3,6 @@
 #include <d3d9.h>
 #include <Windows.h>
 
-struct VmtHook {
-    PVOID base;
-    PUINT_PTR oldVmt;
-    PUINT_PTR newVmt;
-    SIZE_T length;
-};
-
-struct Hooks {
-    HRESULT(WINAPI* originalPresent)(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*);
-    HRESULT(WINAPI* originalReset)(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*);
-    WNDPROC originalWndProc;
-    struct VmtHook clientMode;
-    struct VmtHook panel;
-    struct VmtHook surface;
-};
-
 #ifdef __cplusplus
 extern "C"
 {
