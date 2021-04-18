d3c517efd347a1548a2cea3b1cc1aa6010a7bf70
Daniel Krupiński <danielkrupinski@outlook.com>
2021-04-18 19:58:23 +0200

    Remove C++ support from GUI.h

--- Anubis/GUI.h
+++ Anubis/GUI.h
@@ -3,11 +3,6 @@
 #include <d3d9.h>
 #include <stdbool.h>
 
-#ifdef __cplusplus  
-extern "C"
-{
-#endif
-
 bool GUI_handleInput(HWND, UINT, WPARAM, LPARAM);
 void GUI_init(IDirect3DDevice9*);
 void GUI_render(void);
@@ -15,7 +10,3 @@ void GUI_invalidateDeviceObjects(void);
 void GUI_createDeviceObjects(void);
 
 extern bool isGuiOpen;
-
-#ifdef __cplusplus  
-}
-#endif
